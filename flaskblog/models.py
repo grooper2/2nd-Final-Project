5bea7a42e4f2d5c39adcab222dad769f23b02224
grooper2 <athanasiou454@gmail.com>
2020-02-18 22:46:19 +0000

    First commit

---
+++ flaskblog/models.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.png')
+    password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_name = db.Column(db.String(100), nullable=False)
+    client_project = db.Column(db.Text, nullable=False) 
+    client_or_saggezza = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    billable_to = db.Column(db.String(100), nullable=False)
+    payment = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    receipt = db.Column(db.String(100), nullable=False)    
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text, nullable=False)
+    GBP = db.Column(db.String(20), nullable=True)
+    EUR = db.Column(db.String(20), nullable=True)
+    USD = db.Column(db.String(20), nullable=True)
+    file_name = db.column(db.String(300))
+    receipt_file = db.Column(db.LargeBinary)
+    verify_or_decline =db.Column(db.String(10), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.client_name}', '{self.date_posted}')"
